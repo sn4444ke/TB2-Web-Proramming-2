91ccc5cc2fa69d6660138787a3ed0ea978a97cf5
mugiaadha <48705188+mugiaadha@users.noreply.github.com>
2022-03-31 02:43:29 +0700

    finishing

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -13,12 +13,16 @@ class Dashboard extends BaseController
         $progressPengembalian = $report->getCompleteProgress();
         $earningOverview = $report->getEarningOverview();
         $earningSource = $report->getEarningSource();
+        $bukuDipinjam = $progressPengembalian->buku_dipinjam;
+        $progressPengembalian = ceil(($progressPengembalian->buku_dikembalikan / $progressPengembalian->buku_dipinjam) * 100);
+        $progressPengembalian = $progressPengembalian >= 100 ? 100 : $progressPengembalian ;
+        
 
         return view('DashboardView', [
             'pendapatanBulanan' => "Rp ".number_format($pendapatanBulanan, 0, ',','.'),
             'pendapatanTahunan' => "Rp ".number_format($pendapatanTahunan, 0, ',','.'),
-            'progressPengembalian' => ceil(($progressPengembalian->buku_dikembalikan / $progressPengembalian->buku_dipinjam) * 100),
-            'bukuDipinjam' => $progressPengembalian->buku_dipinjam,
+            'progressPengembalian' => $progressPengembalian,
+            'bukuDipinjam' => $bukuDipinjam,
             'earningOverview' => $earningOverview,
             'earningSource' => $earningSource
         ]);
