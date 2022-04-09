f2f5931c005b57e745cb3b77c47feac2a588e378
KhoirH <hilmi.khoirulloh02@gmail.com>
2022-04-09 23:32:47 +0700

    finish layout

--- app/Views/DashboardView.php
+++ app/Views/DashboardView.php
@@ -1,27 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<?= $this->extend('layout/BaseView') ?>
 
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Perpustakaan - Dashboard</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="<?php echo base_url() ?>/template/libs/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="<?php echo base_url() ?>/template/css/font.css" rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="<?php echo base_url() ?>/template/css/sb-admin-2.min.css" rel="stylesheet">
-
-</head>
-
-<body id="page-top">
-
+<?= $this->section('content') ?>
     <!-- Page Wrapper -->
     <div id="wrapper">
 
@@ -371,23 +350,4 @@
         var earningSourceData = "<?php echo $earningSource['total_pinjam'] ?>".split('::');
     </script>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="<?php echo base_url() ?>/template/libs/jquery/jquery.min.js"></script>
-    <script src="<?php echo base_url() ?>/template/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="<?php echo base_url() ?>/template/libs/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="<?php echo base_url() ?>/template/js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
-    <script src="<?php echo base_url() ?>/template/libs/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="<?php echo base_url() ?>/template/js/demo/chart-area-demo.js"></script>
-    <script src="<?php echo base_url() ?>/template/js/demo/chart-pie-demo.js"></script>
-
-</body>
-
-</html>
+<?= $this->endSection() ?>
--- app/Views/PeminjamanBukuView.php
+++ app/Views/PeminjamanBukuView.php
@@ -1,27 +1,6 @@
+<?= $this->extend('layout/BaseView') ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Perpustakaan - Dashboard</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="<?php base_url() ?>/template/libs/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="<?php base_url() ?>/template/css/font.css" rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="<?php base_url() ?>/template/css/sb-admin-2.min.css" rel="stylesheet">
-</head>
-
-<body id="page-top">
-
+<?= $this->section('content') ?>
     <!-- Page Wrapper -->
     <div id="wrapper">
 
@@ -259,24 +238,4 @@
             </div>
         </div>
     </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="<?php base_url() ?>/template/libs/jquery/jquery.min.js"></script>
-    <script src="<?php base_url() ?>/template/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="<?php base_url() ?>/template/libs/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="<?php base_url() ?>/template/js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
-    <script src="<?php base_url() ?>/template/libs/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="<?php base_url() ?>/template/js/demo/chart-area-demo.js"></script>
-    <script src="<?php base_url() ?>/template/js/demo/chart-pie-demo.js"></script>
-
-</body>
-
-</html>
+<?= $this->endSection() ?>
